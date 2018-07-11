f7f7b42e6ee07c2d4a23ff9a80dff3a32b312f1d
Amadou Gamby <35775591+GamboIII@users.noreply.github.com>
2018-07-11 07:22:48 -0400

    Change machine redone

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,12 @@
-// Author: FirstName LastName
+// Author: Amadou Gamby
+let readline = require("readline-sync");
+console.log("bLeep blOop! i'm yOuR WorST niGhTMarE!");
+
+//user enters amount of change
+let change = parseInt(readline.question("Please enter cents as a positive integer: "));
+console.log(change + " cents makes:");
+console.log(Math.floor(change / 25) + " quarters");
+console.log((Math.floor((change % 25) / 10)) + " dimes");
+console.log((Math.floor((change % 25) % 10) / 5) + " nickels");
+console.log((Math.floor((change % 25) % 10) % 5) + " pennies");
+console.log("BLOop BleEp! i Hope yOU aRE SATiSfieD wITh thiS SerVicE!");
--- super-easter-predictor.js
+++ super-easter-predictor.js
@@ -1,8 +1,1 @@
 // Author: Amadou Gamby
-let readline = require("readline-sync");
-console.log("bLeep blOop! i'm yOuR WorST niGhTMarE!");
-
-//user enters amount of change
-let change = parseInt(readline.question("Please enter cents as a positive integer: "));
-console.log(change + "cents makes:");
-console.log(Math.floor(change / 25) + "quarters");
