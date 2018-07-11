5990a30e546f7e8f68fb48f4e2b5787883205530
Amadou Gamby <35775591+GamboIII@users.noreply.github.com>
2018-07-11 16:13:59 -0400

    Lesson 5 projects complete

--- perfect-square.js
+++ perfect-square.js
@@ -2,7 +2,7 @@ let readline = require("readline-sync");
 
 let userNum = parseFloat(readline.question("Please enter a number: "));
 if (userNum % Math.sqrt(userNum) == 0) {
-  console.log(userNum + " is a perfect square.");
+  console.log(userNum + " is a a perfect square and its root is " + Math.sqrt(userNum) + ".");
 }else {
   console.log(userNum + " isn't a perfect square.");
 }
