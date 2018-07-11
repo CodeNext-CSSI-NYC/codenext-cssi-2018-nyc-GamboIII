3066c410db4dede16d17480178bff34327bd569f
Amadou Gamby <35775591+GamboIII@users.noreply.github.com>
2018-07-11 07:42:13 -0400

    Super Easter predictor

--- easter-predictor.js
+++ easter-predictor.js
@@ -1,18 +1,20 @@
-// Amadou Gamby
-y = 1666;
-a = y % 19;
-b = y / 100;
-c = y % 100;
-d = b / 4;
-e = b % 4;
-f = (b + 8) / 25;
-g = (b - f + 1) / 3;
-h = (19 * a + b - d - g + 1) / 30;
-i = c / 4;
-k = c % 4;
-r = (32 + 2 * e + 2 * i - h - k) % 7;
-m = (a + 11 * h + 22 * r) * 451;
-n = (h + r - 7 * m +114) / 31;
-n = (h + r - 7 * m +114) % 31;
+//Author: Not Amadou Gamby
+let y = 1984;
+let a = y % 19;
+let b = Math.floor(y / 100);
+let c = y % 100;
+let d = Math.floor(b / 4);
+let e = b % 4;
+let f = Math.floor((b + 8) / 25);
+let g = Math.floor((b - f + 1) / 3);
+let h = (19 * a + b - d - g + 15) % 30;
+let i = Math.floor(c / 4);
+let k = c % 4;
+let r = (32 + 2 * e + 2 * i - h - k) % 7;
+let m = Math.floor((a + 11 * h + 22 * r) / 451);
+let n = Math.floor((h + r - 7 * m + 114) / 31);
+let p = (h + r - 7 * m + 114) % 31;
+let day = p + 1;
+let month = n;
 
-console.log("In " + y + " , Easter falls on " 4/20.);
+console.log("This year Easter is on " + month + "/" + day);
