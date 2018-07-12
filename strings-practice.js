64714f207ca4a6b52c33213126b5c626a11a9e17
Amadou Gamby <35775591+GamboIII@users.noreply.github.com>
2018-07-12 16:37:18 -0400

    Every project preceding fantasy-name-generator is complete

--- strings-practice.js
+++ strings-practice.js
@@ -35,6 +35,6 @@ function lastTwo(str) {
   if (str.length < 2) {
     return str;
   }else {
-    return str.substring(0, str.length - 2) + str.charAt(str.length - 1) + str.charAt(str.length - 2)
+    return str.substring(0, str.length - 2) + str.charAt(str.length - 1) + str.charAt(str.length - 2);
   }
 }
